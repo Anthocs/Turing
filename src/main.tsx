acb00c99694c0e965c9997a0886a90c83f8e6968
Anthocs <100413757+Anthocs@users.noreply.github.com>
2025-04-13 11:26:39 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TestDeTuring from './TestDeTuring';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TestDeTuring />
+  </React.StrictMode>
+);
